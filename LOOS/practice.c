803a9b46a2c94d75dc389f32cbc12d633a5701e5
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-20 20:16:19 +0530

    12..

--- LOOS/practice.c
+++ LOOS/practice.c
@@ -1,33 +1,80 @@
+// #include <stdio.h>
+// #define MAX 100
+// void merge(int arr[], int low, int mid, int high)
+// {
+// int i = low;
+// int j = mid + 1;
+// int k = low;
+// int temp[100];
+// while(i <= mid && j <= high)
+// {
+// if(arr[i] <= arr[j])
+// temp[k++] = arr[i++];
+// else
+// temp[k++] = arr[j++];
+// }
+// while(i <= mid)
+// temp[k++] = arr[i++];
+// while(j <= high)
+// temp[k++] = arr[j++];
+// for(i = low; i <= high; i++)
+// arr[i] = temp[i];
+// }
+// void mergeSort(int arr[], int low, int high)
+// {
+// if(low < high)
+// {
+// int mid = (low + high) / 2;
+// mergeSort(arr, low, mid);
+// mergeSort(arr, mid + 1, high);
+// merge(arr, low, mid, high);
+// }
+// }
+// int main()
+// {
+// int n, i;
+// printf("Enter number of elements: ");
+// scanf("%d", &n);
+// int arr[MAX];
+// printf("Enter elements:\n");
+// for(i = 0; i < n; i++)
+// scanf("%d", &arr[i]);
+// mergeSort(arr, 0, n - 1);
+// printf("Sorted array:\n");
+// for(i = 0; i < n; i++)
+// printf("%d ", arr[i]);
+// return 0;
+// }/
 #include <stdio.h>
 #define MAX 100
-void merge(int arr[], int low, int mid, int high)
+void swap(int *a, int *b)
 {
-int i = low;
-int j = mid + 1;
-int k = low;
-int temp[100];
-while(i <= mid && j <= high)
+int temp = *a;
+*a = *b;
+*b = temp;
+}
+int partition(int arr[], int low, int high)
 {
-if(arr[i] <= arr[j])
-temp[k++] = arr[i++];
-else
-temp[k++] = arr[j++];
+int pivot = arr[high];
+int i = low - 1;
+for(int j = low; j < high; j++)
+{
+if(arr[j] < pivot)
+{
+i++;
+swap(&arr[i], &arr[j]);
 }
-while(i <= mid)
-temp[k++] = arr[i++];
-while(j <= high)
-temp[k++] = arr[j++];
-for(i = low; i <= high; i++)
-arr[i] = temp[i];
 }
-void mergeSort(int arr[], int low, int high)
+swap(&arr[i + 1], &arr[high]);
+return i + 1;
+}
+void quickSort(int arr[], int low, int high)
 {
 if(low < high)
 {
-int mid = (low + high) / 2;
-mergeSort(arr, low, mid);
-mergeSort(arr, mid + 1, high);
-merge(arr, low, mid, high);
+int pi = partition(arr, low, high);
+quickSort(arr, low, pi - 1);
+quickSort(arr, pi + 1, high);
 }
 }
 int main()
@@ -39,9 +86,10 @@ int arr[MAX];
 printf("Enter elements:\n");
 for(i = 0; i < n; i++)
 scanf("%d", &arr[i]);
-mergeSort(arr, 0, n - 1);
+quickSort(arr, 0, n - 1);
 printf("Sorted array:\n");
 for(i = 0; i < n; i++)
 printf("%d ", arr[i]);
 return 0;
 }
+
