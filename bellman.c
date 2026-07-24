fe047d2292ce0584d8ca0b9e91e691119b08c862
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-24 12:24:39 +0530

    1..

--- bellman.c
+++ bellman.c
@@ -41,28 +41,25 @@ void bellmanFord(struct Graph* graph, int source) {
     int E = graph->E;
     int dist[V];
 
-    // Step 1: Initialize distances from source to all other vertices as INFINITY
     for (int i = 0; i < V; i++) {
         dist[i] = INT_MAX;
     }
     dist[source] = 0;
 
-    // Step 2: Relax all edges |V| - 1 times.
     for (int i = 1; i <= V - 1; i++) {
         for (int j = 0; j < E; j++) {
             int u = graph->edge[j].source;
             int v = graph->edge[j].destination;
             int weight = graph->edge[j].weight;
             
-            // If the source vertex distance is not INF and the new path is shorter
+     
             if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
                 dist[v] = dist[u] + weight;
             }
         }
     }
 
-    // Step 3: Check for negative-weight cycles. 
-    // If we can still find a shorter path, then a cycle exists.
+ 
     for (int i = 0; i < E; i++) {
         int u = graph->edge[i].source;
         int v = graph->edge[i].destination;
