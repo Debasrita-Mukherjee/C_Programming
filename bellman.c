fda772389feb41b2116c5971747f4be9c384c3d6
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-25 08:08:05 +0530

    3..

--- bellman.c
+++ bellman.c
@@ -91,27 +91,22 @@ int main() {
     graph->edge[2].destination = 3;
     graph->edge[2].weight = 3;
 
-    // Adding edge 1-3
     graph->edge[3].source = 1;
     graph->edge[3].destination = 4;
     graph->edge[3].weight = 2;
 
-    // Adding edge 1-4
     graph->edge[4].source = 3;
     graph->edge[4].destination = 2;
     graph->edge[4].weight = 5;
 
-    // Adding edge 3-2
     graph->edge[5].source = 3;
     graph->edge[5].destination = 1;
     graph->edge[5].weight = 1;
 
-    // Adding edge 4-3
     graph->edge[6].source = 4;
     graph->edge[6].destination = 3;
     graph->edge[6].weight = -3;
-    
-    // Adding edge 2-4
+
     graph->edge[7].source = 1;
     graph->edge[7].destination = 2;
     graph->edge[7].weight = 3;
