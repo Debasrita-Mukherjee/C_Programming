ff7ca5af1c95dcb55f362e2c60f8ebea46b7f05c
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-07 22:07:05 +0530

    1.

--- Halfofinteger.c
+++ Halfofinteger.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 int main(){
-    // int x;
-    // printf("Enter a number : ");
-    // scanf("%d", &x);
-    // float x2 = x;
-    // float y = x2/2;
-    // printf("%f",y);
+    
     float x = 5.7;
     int y = x;
     printf("%d\n",y);
