a185ca615493a78753a8a9292abdffe40ae7ac61
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-21 22:44:34 +0530

    3

--- pattern6.c
+++ pattern6.c
@@ -8,20 +8,18 @@ int main() {
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
-        // Print decreasing numbers
         for (int j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
         printf("\n");
     }
 
-    // Lower Half of the Pattern
     for (int i = rows - 1; i >= 1; i--) {
-        // Print increasing numbers
+       
         for (int j = 1; j <= i; j++) {
             printf("%d", j);
         }
-        // Print decreasing numbers
+    
         for (int j = i - 1; j >= 1; j--) {
             printf("%d", j);
         }
